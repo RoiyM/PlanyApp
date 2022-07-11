aa3f9db3e3a6c88fa0582bfb333e03f2b8a7b587
roiy <roiy355@gmail.com>
2022-07-11 21:58:47 +0300

    added drawer nav.

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
+import "react-native-gesture-handler";
 import * as React from "react";
 import { View, Text } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "./src/screens/HomeScreen";
 import SignupScreen from "./src/screens/SignupScreen";
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
-function App() {
+export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Signup" component={SignupScreen} />
-      </Stack.Navigator>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Signup" component={SignupScreen} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
 
-export default App;
+// const Stack = createNativeStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="Home">
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//         <Stack.Screen name="Signup" component={SignupScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// export default App;
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Signup Screen</Text>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
     </View>
   );
 };
