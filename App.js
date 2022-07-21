25479f6798d930c50778e5d01e3ab4c4eeb6dc96
Noy <noycohen133@gmail.com>
2022-07-21 11:50:25 +0300

    update AboutScreen and create a Logo component

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import * as React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { NavigationContainer } from "@react-navigation/native";
 
-
 import HomeScreen from "./src/screens/HomeScreen";
 import SignupScreen from "./src/screens/SignupScreen";
 import AboutScreen from "./src/screens/AboutScreen";
@@ -11,6 +10,7 @@ import SigninScreen from "./src/screens/SigninScreen";
 import HowItWorksScreen from "./src/screens/HowItWorksScreen";
 import ContactUsScreen from "./src/screens/ContactUsScreen";
 import CreateFloorplanScreen from "./src/screens/CreateFloorplanScreen";
+import DemoScreen from "./src/screens/DemoScreen";
 
 const Drawer = createDrawerNavigator();
 
@@ -19,9 +19,13 @@ export default function App() {
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Home">
         <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="About" component={AboutScreen}/>
+        <Drawer.Screen name="Demo" component={DemoScreen} />
         <Drawer.Screen name="HowItWorks" component={HowItWorksScreen} />
-        <Drawer.Screen name="CreateFloorplan" component={CreateFloorplanScreen} />
+        <Drawer.Screen
+          name="CreateFloorplan"
+          component={CreateFloorplanScreen}
+        />
+        <Drawer.Screen name="About" component={AboutScreen} />
         <Drawer.Screen name="ContactUs" component={ContactUsScreen} />
         <Drawer.Screen name="Signin" component={SigninScreen} />
         <Drawer.Screen name="Signup" component={SignupScreen} />
--- src/components/ImagaDetail.js
+++ src/components/ImagaDetail.js
@@ -3,23 +3,33 @@ import { View, Text, StyleSheet, Image } from "react-native";
 
 const ImageDetail = ({ imageSource, fullName, position, experience }) => {
   return (
-    <View>
+    <View style={styles.container}>
       <Image source={imageSource} style={styles.image} />
       <Text style={styles.full_name}>{fullName}</Text>
-      <Text>{position}</Text>
-      <Text>{experience}</Text>
+      <Text style={styles.text}>{position}</Text>
+      <Text style={styles.text}>{experience}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    justifycontent: "center",
+  },
   image: {
-    height: 150,
-    width: 150,
+    alignSelf: "center",
   },
   full_name: {
-    fontSize: 15,
+    fontSize: 20,
     fontWeight: "bold",
+    textAlign: "center",
+    paddingTop: 10,
+  },
+  text: {
+    textAlign: "center",
+    paddingLeft: 20,
+    paddingRight: 20,
+    paddingBottom: 20,
   },
 });
 
