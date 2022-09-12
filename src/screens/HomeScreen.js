d772eed6cb7539cab660b18733fadc281d69e13a
roiy <roiy355@gmail.com>
2022-09-12 06:32:19 +0300

    fixed style in home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { SafeAreaView } from "react-native";
+import { SafeAreaView, ScrollView } from "react-native";
 import Logo from "../components/Logo";
 import ImageSwapper from "../components/ImageSwapper";
 import PlanYButton from "../components/PlanYButton";
@@ -10,32 +10,34 @@ const planYpink = "#ff005de6";
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <SafeAreaView style={commonStyles.viewContainer}>
-      <Logo />
-      <CustomText style={commonStyles.text}>
-        New{" "}
-        <CustomText style={{ color: planYpink }}>
-          floor plans options
-        </CustomText>{" "}
-        <CustomText>in a few steps</CustomText>
-      </CustomText>
-      <PlanYButton
-        buttonText={"GET STARTED"}
-        onPress={() => {
-          auth.currentUser
-            ? navigation.navigate("Create Floor Plan")
-            : navigation.navigate("Sign In");
-        }}
-      />
-      <ImageSwapper />
-      <CustomText style={commonStyles.text}>
-        <Logo fontSize={21} />
-        ourself - architecture platform
-      </CustomText>
-      <CustomText style={commonStyles.smallText}>
-        Get architectural services at a click of a button!
-      </CustomText>
-    </SafeAreaView>
+    <ScrollView contentContainerStyle={commonStyles.scrollViewContainer}>
+      <SafeAreaView style={commonStyles.inner}>
+        <Logo />
+        <CustomText style={commonStyles.text}>
+          New{" "}
+          <CustomText style={{ color: planYpink }}>
+            floor plans options
+          </CustomText>{" "}
+          <CustomText>in a few steps</CustomText>
+        </CustomText>
+        <PlanYButton
+          buttonText={"GET STARTED"}
+          onPress={() => {
+            auth.currentUser
+              ? navigation.navigate("Create Floor Plan")
+              : navigation.navigate("Sign In");
+          }}
+        />
+        <ImageSwapper />
+        <CustomText style={commonStyles.text}>
+          <Logo fontSize={21} />
+          ourself - architecture platform
+        </CustomText>
+        <CustomText style={commonStyles.smallText}>
+          Get architectural services at a click of a button!
+        </CustomText>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
